5fd3780b05c0ec2f789906a7882bfdfeb30e2770
lorenzo bellino <lor.bellino@gmail.com>
2022-10-24 11:23:25 +0200

    Issue 3: added precomputing to remove duplicates and empty lists

--- lab1/setCovering.py
+++ lab1/setCovering.py
@@ -1,6 +1,7 @@
 import random
-from queue import PriorityQueue
 import logging
+from itertools import groupby
+from queue import PriorityQueue
 from time import perf_counter_ns
 from math import ceil
 
@@ -8,15 +9,17 @@ SEED = 42
 
 logging.basicConfig(format="%(message)s", level=logging.INFO)
 
+
 class PrioritizedSet:
-    '''A set with a priority
+    """A set with a priority
     ...
     Attributes :
         priority : int
             The inverse priority of the set (smaller is higer priority)
         item : set
             The set
-    '''
+    """
+
     def __init__(self, priority: int, set_: set):
         self.priority = 100 - priority
         self.item = set_
@@ -32,7 +35,7 @@ class PrioritizedSet:
 
 
 def problem(N, seed=None):
-    '''Generate a random problem given a seed
+    """Generate a random problem given a seed
     ...
     Attributes :
         N : int
@@ -40,15 +43,16 @@ def problem(N, seed=None):
         seed : int
             The seed for the random number generator
             default value = 42
-    '''
+    """
     random.seed(seed)
     return [
-        list(set(random.randint(0,N-1) for n in range(random.randint(N // 5, N // 2))))
-        for n in range(random.randint(N,N*5))
-        ]
+        list(set(random.randint(0, N - 1) for n in range(random.randint(N // 5, N // 2))))
+        for n in range(random.randint(N, N * 5))
+    ]
 
-def calculate_weight(result,goal,set_ = set(),threshold=1):
-    '''Calculate the weight of a set
+
+def calculate_weight(result, goal, set_=set(), threshold=1):
+    """Calculate the weight of a set
     ...
     Attributes :
         result : set
@@ -61,12 +65,14 @@ def calculate_weight(result,goal,set_ = set(),threshold=1):
         threshold : float
             The threshold for the weight
             default value = 1
-    '''
-    weight = ceil(100*sum(x in goal-result for x in set_) / len(set_))
-    return 100 if weight >= threshold*100 else weight
+    """
+    # weight = the percentage of new elements covered by the set in respect to the state of the solution
+    weight = ceil(100 * sum(x in goal - result for x in set_) / len(set_))
+    return 100 if weight >= threshold * 100 else weight
 
-def search(sets,goal,N,threshold=1):
-    '''Search for a solution to the set covering problem
+
+def search(sets, goal, N, threshold=1):
+    """Search for a solution to the set covering problem
     ...
     Attributes :
         sets : list
@@ -78,19 +84,24 @@ def search(sets,goal,N,threshold=1):
         threshold : float
             The threshold for the weight
             default value = 1
-    '''
+    """
     discovered_state = 0
     options = PriorityQueue()
     unused = PriorityQueue()
+    # added a precomputing step:
+    # remove empty lists and remove duplicates
+    sets = [s for s in sets if s]
+    sets = list(sets for sets, _ in groupby(sets))
+
     for element in sets:
-        options.put(PrioritizedSet(int(100*len(element)/N),element))
+        options.put(PrioritizedSet(int(100 * len(element) / N), element))
     result = [options.get().item]
     result_set = set().union(result[0])
     while result is not None and not result_set == goal:
         while not options.empty():
             discovered_state += 1
             s = options.get().item
-            coverage = calculate_weight(result_set,goal,s,threshold)
+            coverage = calculate_weight(result_set, goal, s, threshold)
             if coverage == 100:
                 result.append(s)
                 result_set = result_set.union(s)
@@ -98,7 +109,7 @@ def search(sets,goal,N,threshold=1):
                     options.put(unused.get())
                 break
             if coverage != 0:
-                unused.put(PrioritizedSet(coverage,s))
+                unused.put(PrioritizedSet(coverage, s))
         else:
             if unused.empty():
                 result = None
@@ -111,14 +122,15 @@ def search(sets,goal,N,threshold=1):
     logging.info(f"explored state: {discovered_state}")
     return result
 
+
 def main():
-    '''Main function'''
-    for n in [5,10,20,50,100,500,1000]:
-        sets = problem(n,SEED)
+    """Main function"""
+    for n in [5, 10, 20, 50, 100, 500, 1000]:
+        sets = problem(n, SEED)
         goal = set(_ for _ in range(n))
         logging.info(f"N = {n}")
         start_time_ns = perf_counter_ns()
-        result = search(sets, goal,n,threshold=0.5)
+        result = search(sets, goal, n, threshold=0.5)
         end_time_ns = perf_counter_ns()
         logging.info(f"Time: {end_time_ns-start_time_ns} ns")
         if result is None:
@@ -126,6 +138,6 @@ def main():
         else:
             logging.info(f"the weight of the solution is: {sum(len(s) for s in result)}")
 
+
 if __name__ == "__main__":
     main()
-    
