fef05e542956ed53f23f1a33b52483867cea6098
lorenzo bellino <lor.bellino@gmail.com>
2022-11-16 17:03:57 +0100

    commented and cleaned solution

--- lab2/setCovering.py
+++ lab2/setCovering.py
@@ -1,8 +1,7 @@
-import random
 import logging
+import random
 from collections import namedtuple
 
-# PROBLEM_SIZE = 500
 POPULATION_SIZE = 300
 OFFSPRING_SIZE = 200
 
@@ -10,14 +9,8 @@ NUM_GENERATIONS = 100
 
 DEBUG = False
 logging.basicConfig(level=logging.DEBUG if DEBUG else logging.INFO)
-
+# basic data structure for an individual in order to access its parameters
 Individual = namedtuple("Individual", ["genome", "fitness"])
-
-
-def printPop(population):
-    for individual in population:
-        # logging.debug(f"Genome: {individual.genome}, Fitness: {individual.fitness}")
-        logging.debug(f"Fitness: {individual.fitness}")
 
 
 def problem(N, seed=None):
@@ -30,20 +23,45 @@ def problem(N, seed=None):
 
 
 def fitness(genome, N, maxlen):
+    """
+    calculate the fitness of a genome
+    param genome: the genome to calculate the fitness of
+    param N: the size of the problem
+    param maxlen: the maximum length of a list
+    return: the fitness of the genome
+    the fitness is a tuple containing:
+        the actual fitness calculated as covered elements - a penalty for duplicates
+        the number of covered elements
+        the number of duplicates as a negative value (the less duplicates the better)
+    """
+
     covered = len(set(loci for gene in genome for loci in gene))
     duplicates = len([loci for gene in genome for loci in gene]) - covered
     fitness = covered - (duplicates * N / maxlen)
-    # fitness = covered - duplicates
-    # return (covered, -duplicates)
     return (fitness, covered, -duplicates)
 
 
 def tournament(population, tournament_size=2):
+    """
+    default tournament with k = 2
+    select a random individual from the population
+    and compare it with another random individual
+    return the fittest individual
+    """
     return max(random.choices(population, k=tournament_size), key=lambda i: i.fitness)
 
 
 def cross_over(g1, g2):
-    """Crossover two genomes by slicing them at a random point"""
+    """
+    Crossover two genomes by slicing them at a random point
+    param g1: genome 1
+    param g2: genome 2
+    return: a new genome
+    with probabiiity 0.5 the first part of g1 is combined with the second part of g2
+    with probability 0.5 the first part of g2 is combined with the second part of g1
+    with probability 0.3 the genome is shortened by a random amount
+    """
+    logging.debug(f"crossover lenghts : {len(g1)} and {len(g2)}")
     cut = random.randint(0, min(len(g1), len(g2)))
     if random.random() < 0.5:
         o = g1[:cut] + g2[cut:]
@@ -55,6 +73,16 @@ def cross_over(g1, g2):
 
 
 def mutation(g, all_lists):
+    """
+    mutate a genome by adding a random gene from all_lists
+    param g: genome to mutate
+    param all_lists: list of all genes
+    return: mutated genome
+    with probsbility 0.3 a random list is added to the genome
+    with probability 0.7 a random list is substituted with another random list
+    with a probability of 0.3 the genome is shortened by a random amount
+    """
+    logging.debug(f"mutation lenght : {len(g)}")
     point = random.randint(0, len(g) - 1)
     if random.random() < 0.3:
         o = g[:point] + (random.choice(all_lists),) + g[point:]
@@ -66,6 +94,14 @@ def mutation(g, all_lists):
 
 
 def generate_population(N, all_lists, maxlen):
+    """
+    generate a population of POPULATION_SIZE individuals
+    param N: the size of the problem
+    param all_lists: the list of all possible lists
+    param maxlen: the maximum length of a list
+    return:
+        a list of POPULATION_SIZE individuals
+    """
     population = list()
     for genome in [
         tuple([random.choice(all_lists) for _ in range(random.randint(1, N // 3))]) for _ in range(POPULATION_SIZE)
@@ -75,6 +111,16 @@ def generate_population(N, all_lists, maxlen):
 
 
 def generate_offspring(population, all_lists, N, maxlen):
+    """
+    param population: the current population
+    param all_lists: the list of all possible lists
+    param N: the size of the problem
+    param maxlen: the maximum length of a list
+    return: a list of offspring
+
+    apply mutation with a probability of 0.4
+    apply crossover with a probability of 0.6
+    """
     offspring = list()
     for i in range(OFFSPRING_SIZE):
         if random.random() < 0.4:
@@ -84,34 +130,32 @@ def generate_offspring(population, all_lists, N, maxlen):
             p1 = tournament(population)
             p2 = tournament(population)
             o = cross_over(p1.genome, p2.genome)
-        f = fitness(o, N, maxlen)
-
-        offspring.append(Individual(o, f))
+        # sometimes crossover or mutation can produce an empty genome
+        if len(o) > 0:
+            f = fitness(o, N, maxlen)
+            offspring.append(Individual(o, f))
     return offspring
 
 
 def combine(population, offspring):
+    """
+    combine the current population and a new one into a new population
+    """
     population += offspring
     population = sorted(population, key=lambda i: i.fitness, reverse=True)[:POPULATION_SIZE]
     return population
 
 
-# def reshuflle(population, all_lists, N, maxlen):
-#     new_population = list()
-#     for i in population:
-#         new_len = random.randint(1, len(i.genome))
-#         # logging.info(f"new_len: {new_len}")
-#         try:
-#             o = tuple(random.sample(i.genome, new_len))
-#         except ValueError:
-#             logging.info(f"ValueError: {i.genome}")
-#             # o = i.genome
-#         new_population.append(Individual(o, fitness(o, N, maxlen)))
-#     return new_population
-
-
 def setCovering(N, all_lists):
-    """Solve the set covering problem using a genetic algorithm"""
+    """
+    Solve the set covering problem using a genetic algorithm
+    for each generation generate offspring and combine them with the population
+    and then select the best individuals
+    if a steady state is reached where the best_fitness doesn't change
+    for 7 iteration than a reshuffle and combination with a new population is performed
+    return:
+        the fittest individual after NUM_GENERATIONS
+    """
     maxlen = sum(len(l) for l in all_lists)
     population = generate_population(N, all_lists, maxlen)
     bestfit = population[0].fitness
@@ -127,21 +171,17 @@ def setCovering(N, all_lists):
             bestfit = population[0].fitness
             steady = 0
         if steady == 7:
-            # population = reshuflle(population, all_lists, N, maxlen)
             population = combine(population, generate_population(N, all_lists, maxlen))
-            # population = generate_population(N, all_lists, maxlen)
             steady = 0
-        # logging.info(f"Best fitness: {population[0].fitness}")
-        printPop(population)
 
     return population[0].genome
 
 
 def main():
     """
-    This is not the final solution since i was sick and i could not finish it
-    however this is a good proof of concept
-    but it still needs a lot of work i'm sorry for the mess
+    main function used to iterate over the various problem space
+    for N = [5, 10, 20, 50, 100, 200, 500, 1000]
+    and logging each result found
     """
     solutions = list()
     for N in [5, 10, 20, 50, 100, 200, 500, 1000]:
